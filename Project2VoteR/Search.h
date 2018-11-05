1077fa7ebd02b45627676ad88c2b1900460e4b34
josh-butts <32021460+josh-butts@users.noreply.github.com>
2018-11-05 13:56:13 -0600

    79/80

--- Project2VoteR/Search.h
+++ Project2VoteR/Search.h
@@ -10,24 +10,24 @@
 // E.g. if it belongs at index zero, return -1, or index 2, return -3
 template <typename T>
 long long binarySearch(const T& item, const TemplatedArray<T>& array, const Comparator<T>& comparator) {
-	unsigned long high = array.getCapacity() - 1;
-	unsigned long low = 0;
+	long high = array.getSize() - 1;
+	long low = 0;
 	unsigned long mid;
 	while (high >= low)
 	{
 		mid = (high + low) / 2;
-		if (comparator.compare(item, array[mid]) == -1) // it's less than mid
+		if (comparator.compare(item, array.get(mid)) == -1) // it's less than mid
 		{
 			high = mid - 1;
 		}
-		else if (comparator.compare(item, array[mid]) == 1) // it's greater than mid
+		else if (comparator.compare(item, array.get(mid)) == 1) // it's greater than mid
 		{
 			low = mid + 1;
 		}
 		else // it's found
 		{
 			// find the first instance of the found object
-			while (mid != 0 && comparator.compare(array[mid], array[mid - 1]) == 0) // if it's the same as the one before it
+			while (mid != 0 && comparator.compare(array.get(mid), array.get(mid - 1)) == 0) // if it's the same as the one before it
 			{
 				mid = mid - 1; // move the index back one
 			}
