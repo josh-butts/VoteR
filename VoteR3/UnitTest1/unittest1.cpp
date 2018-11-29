01ce2e2f2279194f742bccdf00d5602aef4a5114
josh-butts <32021460+josh-butts@users.noreply.github.com>
2018-11-29 15:41:47 -0600

    test thingy

---
+++ VoteR3/UnitTest1/unittest1.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Exceptions.h"
+#include "TemplatedArray.h"
+#include "NvraRecord.h"
+#include "NvraComparator.h"
+#include "Sorter.h"
+#include "Search.h"
+#include "OULink.h"
+#include "OULinkedList.h"
+#include "OULinkedListEnumerator.h"
+#include "Enumerator.h"
+#include "HashTable.h"
+#include "NvraHasher.h"
+#include "HashTableEnumerator.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
