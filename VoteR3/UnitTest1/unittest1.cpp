9e58b9d74270c196b19d7a9396a1791cc928214b
josh-butts <32021460+josh-butts@users.noreply.github.com>
2018-11-29 15:42:26 -0600

    testing bullshit

--- VoteR3/UnitTest1/unittest1.cpp
+++ VoteR3/UnitTest1/unittest1.cpp
@@ -1,22 +1,23 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "Exceptions.h"
-#include "TemplatedArray.h"
-#include "NvraRecord.h"
-#include "NvraComparator.h"
-#include "Sorter.h"
-#include "Search.h"
-#include "OULink.h"
-#include "OULinkedList.h"
-#include "OULinkedListEnumerator.h"
-#include "Enumerator.h"
-#include "HashTable.h"
-#include "NvraHasher.h"
-#include "HashTableEnumerator.h"
+#include "../VoteR3/Comparator.h"
+#include "../VoteR3/Enumerator.h"
+#include "../VoteR3/Exceptions.h"
+#include "../VoteR3/TemplatedArray.h"
+#include "../VoteR3/NvraRecord.h"
+#include "../VoteR3/NvraComparator.h"
+#include "../VoteR3/Sorter.h"
+#include "../VoteR3/Search.h"
+#include "../VoteR3/OULink.h"
+#include "../VoteR3/OULinkedList.h"
+#include "../VoteR3/OULinkedListEnumerator.h"
+#include "../VoteR3/Enumerator.h"
+#include "../VoteR3/HashTable.h"
+#include "../VoteR3/NvraHasher.h"
+#include "../VoteR3/HashTableEnumerator.h"
 #include <iostream>
 #include <fstream>
 #include <string>
-
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
