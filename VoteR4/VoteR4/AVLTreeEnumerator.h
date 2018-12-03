3628474a93bd93ff3bdb36ba4358776f70321c00
josh-butts <32021460+josh-butts@users.noreply.github.com>
2018-12-03 13:38:57 -0600

    setup done

--- VoteR4/VoteR4/AVLTreeEnumerator.h
+++ VoteR4/VoteR4/AVLTreeEnumerator.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef AVL_TREE_ENUMERATOR
+#define AVL_TREE_ENUMERATOR
+
+#include <stack>
+#include "Enumerator.h"
+#include "AVLTreeOrder.h"
+#include "AVLTree.h"
+
+template <typename T>
+class AVLTreeEnumerator : public Enumerator<T>
+{
+private:
+	AVLTreeOrder order;
+	std::stack<const AVLTree<T>*> traversalStack;
+	void buildTraversalStack(const AVLTree<T>* current);
+public:
+	AVLTreeEnumerator(const AVLTree<T>* root, AVLTreeOrder order);
+	virtual ~AVLTreeEnumerator();
+	bool hasNext() const;
+	T next();						// throws ExceptionEnumerationBeyondEnd if no next item is available
+	T peek() const;					// throws ExceptionEnumerationBeyondEnd if no next item is available
+	AVLTreeOrder getOrder();		// returns the order of this enumerator (preorder, inorder, or postorder)
+};
+
+// put implementation here
+
+#endif // !AVL_TREE_ENUMERATOR
--- VoteR4/VoteR4/AVLTreeOrder.h
+++ VoteR4/VoteR4/AVLTreeOrder.h
@@ -1,1 +1,6 @@
-#pragma once
+#ifndef AVL_TREE_ORDER
+#define AVL_TREE_ORDER
+
+enum class AVLTreeOrder : int { preorder, inorder, postorder };
+
+#endif // !AVL_TREE_ORDER
